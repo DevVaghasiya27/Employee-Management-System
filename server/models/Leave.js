387ac902219c562d1eafd6268ebbf01782a772b1
Dev Vaghasiya <135097978+DevVaghasiya27@users.noreply.github.com>
2025-03-28 13:18:55 +0530

    Update Schema of Leave.js

--- server/models/Leave.js
+++ server/models/Leave.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
+
 const leaveSchema = new Schema({
-    employeeId: { type: Schema.Types.ObjectId, ref: "Employee", required: true},
+    employeeId: { type: Schema.Types.ObjectId, ref: "Employee", required: true },
+    employeeName: { type: String, required: true },
     leaveType: {
-        type:String,
+        type: String,
         enum: ["Sick Leave", "Casual Leave", "Annual Leave"],
         required: true,
     },
-    startDate: { type: Date, required: true},
-    endDate: {type: Date, required: true},
-    reason: { type: String, required: true},
-    status: { type: String, enum: ["Pending", "Approved", "Rejected"], default: "Pending"},
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    reason: { type: String, required: true },
+    status: { type: String, enum: ["Pending", "Approved", "Rejected"], default: "Pending" },
     appliedAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
