b71d60e501a39ca1be605fea3bb26141301103ca
Dev Vaghasiya <135097978+DevVaghasiya27@users.noreply.github.com>
2025-03-28 13:20:39 +0530

    Update Routes of employee.js

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js';
-import { addEmployee, getEmployees, upload, getEmployee, updateEmployee, fetchEmployeesByDepId } from '../controllers/employeeController.js';
+import { addEmployee, getEmployees, upload, getEmployee, updateEmployee, fetchEmployeesByDepId, deleteEmployee } from '../controllers/employeeController.js';
 
 const router = express.Router()
 
 router.get('/', authMiddleware, getEmployees)
 router.post('/add', authMiddleware, upload.single('image'), addEmployee)
-router.put('/:id', authMiddleware, updateEmployee)
+router.put('/:id', authMiddleware, upload.single('image'), updateEmployee);
 router.get('/:id', authMiddleware, getEmployee)
 router.get('/department/:id', authMiddleware, fetchEmployeesByDepId)
+router.delete('/:id', authMiddleware, deleteEmployee);
 
 export default router
